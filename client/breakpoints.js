70d7bdc446067896ac66afb0de78035538ddc447
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-05-19 10:06:06 -0700

    change breakpoints

--- client/breakpoints.js
+++ client/breakpoints.js
@@ -9,7 +9,7 @@ module.exports = {
   'screen-300': 300,
   'screen-375': 375,
   'screen-495': 495,
-  'screen-550': 550,
+  'screen-500': 550,
   'screen-560': 560,
   'screen-600': 600,
   'screen-655': 655,
@@ -17,13 +17,4 @@ module.exports = {
   'screen-875': 875,
   'screen-1150': 1150,
   'screen-1425': 1425,
-
-  // TODO Remove when breakpoint existing usages are renamed.
-  xs: 300,
-  sm: 375,
-  md: 495,
-  lg: 600,
-  xl: 875,
-  '2xl': 1150,
-  '3xl': 1425,
 };
